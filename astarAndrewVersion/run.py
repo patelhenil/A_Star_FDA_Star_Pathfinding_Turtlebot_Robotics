81707901506c5d30b5c45f8d7e49e678c50a0603
Andrew Le <andrewledev@gmail.com>
2018-09-29 15:47:09 -0400

    Add files via upload

--- astarAndrewVersion/run.py
+++ astarAndrewVersion/run.py
@@ -51,13 +51,15 @@ c = matrixCornersArr[2].split(',')
 
 d = matrixCornersArr[3].split(',')
 
-offset_x = 0 - int(d[0])
-offset_y = 0 - int(b[1])
+offset_x = 0 - float(d[0])
+offset_y = 0 - float(b[1])
+
+print(offset_y)
 
 for obstacle in obstacleList:
     for point in obstacle:
-        point[0] = int(point[0]) + offset_x
-        point[1] = int(point[1]) + offset_y
+        point[0] = float(point[0]) + offset_x
+        point[1] = float(point[1]) + offset_y
 
         point[0] = point[0] * 2
         point[1] = point[1] * 2
@@ -67,37 +69,43 @@ for obstacle in obstacleList:
         point[1] = temp
 
 for start in startList:
-    start[0] = int(start[0]) + offset_x
-    start[1] = int(start[1]) + offset_y
+    start[0] = float(start[0]) + offset_x
+    start[1] = float(start[1]) + offset_y
     start[0] = start[0] * 2
     start[1] = start[1] * 2
 
 for goal in goalList:
-    goal[0] = int(goal[0]) + offset_x
-    goal[1] = int(goal[1]) + offset_y
+    goal[0] = float(goal[0]) + offset_x
+    goal[1] = float(goal[1]) + offset_y
     goal[0] = goal[0] * 2
     goal[1] = goal[1] * 2
 
-matrixHeight = abs(int(a[1]) - int(b[1]))
-matrixWidth = abs(int(a[0]) - int(d[0]))
+matrixHeight = abs(float(a[1]) - float(b[1]))
+matrixWidth = abs(float(a[0]) - float(d[0]))
 
 matrix = []
 
-for row in range(0, matrixHeight * 2):
+for row in range(0, int(matrixHeight) * 2):
     rowArr = []
-    for col in range(0, matrixWidth * 2):
+    for col in range(0, int(matrixWidth) * 2):
         rowArr.append(1)
     matrix.append(rowArr)
 
-for y in range(0, matrixHeight * 2):
-    for x in range(0, matrixWidth * 2):
+for x in range(0,int(matrixHeight) * 2):
+    matrix[x][0] = 0
+    matrix[int(matrixHeight) * 2-1][x] = 0
+    matrix[x][int(matrixHeight) * 2-1] = 0
+    matrix[0][x] = 0
+
+for y in range(0, int(matrixHeight) * 2):
+    for x in range(0, int(matrixWidth) * 2):
         if y + 1 >= matrixHeight * 2 or x + 1 >= matrixWidth * 2:
             continue
 
 
         # topleft
 
-        '''for obstacle in obstacleList:
+        for obstacle in obstacleList:
             if inside_polygon(x, y, obstacle) or on_polygon(x, y, obstacle):
                 matrix[x][y] = 0
 
@@ -115,14 +123,15 @@ for y in range(0, matrixHeight * 2):
         for obstacle in obstacleList:
             if inside_polygon(x + 1, y + 1, obstacle) or on_polygon(x + 1, y + 1, obstacle):
                 matrix[x][y] = 0
-        '''
 
-        obstacle = obstacleList[0]
+
+        '''obstacle = obstacleList[0]
 
         if inside_polygon(x, y, obstacle) or on_polygon(x, y, obstacle):
             matrix[x][y] = 0
 
         print("({},{}), inside {}, on {}".format(x,y,inside_polygon(x, y, obstacle),on_polygon(x, y, obstacle)))
+        '''
 
 
 
@@ -156,8 +165,27 @@ for y in range(0, matrixHeight * 2):
     ]'''
 grid = Grid(matrix=matrix)
 
-start = grid.node(int(startList[0][0]), int(startList[0][1]))
-end = grid.node(int(goalList[0][0]), int(goalList[0][1]))
+
+'''for x in range(0,len(startList)):
+    start = grid.node(int(startList[x][0]), int(startList[x][1]))
+    end = grid.node(int(goalList[x][0]), int(goalList[x][1]))
+
+    print("start is ({},{}), end is ({},{})".format(int(startList[x][0]),int(startList[x][1]),int(goalList[x][0]),int(goalList[x][1])))
+
+    finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
+    path, runs = finder.find_path(start, end, grid)
+
+    print('operations:', runs, 'path length:', len(path))
+    print(grid.grid_str(path=path, start=start, end=end))
+    print(path)
+'''
+
+x=10
+start = grid.node(int(startList[x][0]), int(startList[x][1]))
+end = grid.node(int(goalList[x][0]), int(goalList[x][1]))
+
+print("start is ({},{}), end is ({},{})".format(int(startList[x][0]), int(startList[x][1]), int(goalList[x][0]),
+                                                int(goalList[x][1])))
 
 finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
 path, runs = finder.find_path(start, end, grid)
@@ -166,15 +194,18 @@ print('operations:', runs, 'path length:', len(path))
 print(grid.grid_str(path=path, start=start, end=end))
 print(path)
 
+
+
+
 str = "x   "
 
-for m in range(0, matrixHeight * 2):
+for m in range(0,int(matrixHeight) * 2):
     str = str + "{}, ".format( int(m))
 
 print(str)
 print()
 
-for m in range(0, matrixHeight * 2):
+for m in range(0, int(matrixHeight) * 2):
     temp = ''
     if m < 10:
         temp = '0'
